a1d5017410bcac9f995f4d46f750331c8028ed14
Alma Gonzalez <agonzalez@grupoabraxas.com>
2019-07-06 16:05:34 -0500

    Get el color del usuario de twitter
    import user_color
    print(user_color.get('SoyTecnopata'))

--- user_color.py
+++ user_color.py
@@ -1,8 +1,12 @@
-import time
-from selenium import webdriver
-import pprint
-import pandas as pd
-#from xvfbwrapper import Xvfb
-import datetime
-import csv
-import os.path
+import re
+import requests
+from bs4 import BeautifulSoup
+
+def get(user):
+    URL = 'https://twitter.com/'
+    r = requests.get(URL+user)
+    soup = BeautifulSoup(r.content, 'html5lib')
+    search_2 = str(soup.find("style"))
+    result = re.search('color: (.*);', search_2)
+    color=result.group(1)
+    return color
