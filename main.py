19abc9fae9eb53a3b2faf802ae6366b23a0d09fa
OscJD <oscar5-juarez@hotmail.com>
2019-07-07 01:44:07 -0500

    main file with pretty prints

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import get_favorites_twitter
 import profile_pic
 import user_color
 import visual_rec_api
+import pprint
 
+pp = pprint.PrettyPrinter(indent=4)
 
 user = "@akirareiko"
+
+pp.pprint("USUARIO: " + user)
 
 page_color = user_color.get(user)
 
@@ -17,15 +21,17 @@ if page_color == None:
 else:
     profile_pic.get(user[1:])
 
-print page_color ### Color que va directo a la pagina
+pp.pprint( "COLOR DEL SITIO: " + str(page_color)) ### Color que va directo a la pagina
 
 tez, gender = visual_rec_api.get_tez_and_gender("./imagenes/" + user[1:] + "/profile_pic.jpg")
 
-print tez, gender #### Color de piel y genero
+pp.pprint( "COLOR DE PIEL: " + str(tez))
+pp.pprint( "GENERO: " + str(gender)) #### Color de piel y genero
 
 if gender == "male":
     labels_for_clothes = get_favorites_twitter.get_tags_from_fav(user)
-    print labels_for_clothes ### Labels para ordenar la ropa de los hombres
+    pp.pprint("CATEGORIAS Y CONCEPTOS:")
+    pp.pprint(labels_for_clothes) ### Labels para ordenar la ropa de los hombres
     
 
     
