64f9b0708b411927b9112efab5ff855726cf23bb
OscJD <oscar5-juarez@hotmail.com>
2019-07-06 20:09:56 -0500

    fix de cositos

--- user_color.py
+++ user_color.py
@@ -4,12 +4,15 @@ from bs4 import BeautifulSoup
 
 def get(user):
     URL = 'https://twitter.com/'
-    r = requests.get(URL+user)
+    r = requests.get(URL+str(user))
     soup = BeautifulSoup(r.content, 'html5lib')
     search_2 = str(soup.find("style"))
-    result = re.search('color: (.*);', search_2)
+    result = re.search('color: #(.*);', search_2)
     try:
         color=result.group(1)
+        
+        color_rgb = tuple(int(color[i:i+2], 16) for i in (0, 2, 4))
+        
     except Exception:
-        color= None
-    return color
+        color_rgb= None
+    return color_rgb
