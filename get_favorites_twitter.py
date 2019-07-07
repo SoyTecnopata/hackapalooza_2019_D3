c72494605bf30d0c1e13d0374041a4c0e74123e6
OscJD <oscar5-juarez@hotmail.com>
2019-07-06 23:15:40 -0500

    tags from natural language understanding added

--- get_favorites_twitter.py
+++ get_favorites_twitter.py
@@ -1,44 +1,55 @@
 import tweepy
-import pandas as pd
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_watson.natural_language_understanding_v1 import Features, ConceptsOptions, CategoriesOptions
 
 consumer_key = "5pkeOivdlWIfnIjQH0D8gLoKk"
 consumer_secret = "d4dZxHQXHlpDVKLwFKmza6wqotAOkQEhmYtB3oujrt6yNXcsEs"
 access_token = "921073038497546240-FihL8EPubRyKvUCbzzQwKuiEkf9hzPd"
 access_token_secret = "VWjeV0ceU4dIYem7aLtWyCpVTa3f8jMfFpHZqNKet75Xo"
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+def get_tags_from_fav(user):
 
-api = tweepy.API(auth)
-
-favorites = api.favorites( "@SoyTecnopata",3)
-
-index = 0
-dic_tweets = dict()
-
-for tweets in favorites:
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
     
-    text_of_tweet = tweets.text.encode("ascii", "ignore").decode("ascii", "ignore")
-
-    dic_tweets[index] = text_of_tweet
+    api = tweepy.API(auth)
     
-    index += 1
+    favorites = api.favorites( user,3)
     
-
-import json
-from ibm_watson import NaturalLanguageUnderstandingV1
-from ibm_watson.natural_language_understanding_v1 import Features, RelationsOptions, CategoriesOptions
-
-natural_language_understanding = NaturalLanguageUnderstandingV1(
-    version='2018-11-16',
-    iam_apikey='zq_JRbDtCInoaWml-ZAjfGFn2Vj2b9wvZzfRE1O5U_wJ',
-    url='https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2018-11-16'
-)
-
-for keys in dic_tweets.keys():
-
-    response = natural_language_understanding.analyze(
+    index = 0
+    dic_tweets = dict()
+    
+    for tweets in favorites:
+        
+        text_of_tweet = tweets.text.encode("ascii", "ignore").decode("ascii", "ignore")
+    
+        dic_tweets[index] = text_of_tweet
+        
+        index += 1
+        
+    
+    natural_language_understanding = NaturalLanguageUnderstandingV1(
+        version='2018-11-16',
+        iam_apikey='zq_JRbDtCInoaWml-ZAjfGFn2Vj2b9wvZzfRE1O5U_wJ',
+        url='https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2018-11-16'
+    )
+    
+    tags = []
+    
+    for keys in dic_tweets.keys():
+    
+        response_categories = natural_language_understanding.analyze(
+            text=dic_tweets[keys],
+            features=Features(categories=CategoriesOptions(limit=5))).get_result()
+        
+        response_concepts = natural_language_understanding.analyze(
         text=dic_tweets[keys],
-        features=Features(categories=CategoriesOptions(limit=5))).get_result()
+        features=Features(concepts=ConceptsOptions(limit=5))).get_result()
+        
+        if len(response_concepts["concepts"]) != 0:
+            for i in range(len(response_concepts["concepts"])):
+                tags.append(response_concepts["concepts"][i]["text"])
+            
+        tags.append(response_categories["categories"][0]["label"].split("/")[-1])
     
-    print(json.dumps(response, indent=2))
+    return tags
